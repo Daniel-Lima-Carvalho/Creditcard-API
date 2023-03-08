8241787f6958d44555bec2296f205197b4552529
Daniel Lima <daniellima2297@gmail.com>
2023-03-08 19:14:32 -0300

    Adding creditcard admin

--- creditcard/admin.py
+++ creditcard/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from creditcard.models import Creditcard
 
-# Register your models here.
+@admin.register(Creditcard)
+class CreditcardAdmin(admin.ModelAdmin):
+    fields = ('exp_date', 'holder', 'number', 'cvv')
+    readonly_fields = ('exp_date', 'holder', 'number', 'cvv')
+    search_fields = ('exp_date', 'holder', 'number', 'cvv')
+    list_display = ('exp_date', 'holder', 'number', 'cvv')
