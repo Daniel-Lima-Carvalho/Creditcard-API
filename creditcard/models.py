1247be7451eb1102f40d47cbbdc455f79b56416e
Daniel Lima <daniellima2297@gmail.com>
2023-03-08 20:06:46 -0300

    Adjusting exp date field

--- creditcard/models.py
+++ creditcard/models.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Creditcard(models.Model):
-    exp_date = models.DateTimeField(verbose_name=_('expiration date'))
+    exp_date = models.DateField(verbose_name=_('expiration date'))
     holder = models.CharField(max_length=500, verbose_name=_('holder'))
     number = models.CharField(max_length=500, verbose_name=_('number'))
     cvv = models.IntegerField(verbose_name=_('cvv'))
